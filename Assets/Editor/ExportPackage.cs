5f259bf53f9bcebc1e846d15f12ee5bf6d626f28
酒井峰志 <sakai861104@gmail.com>
2017-11-28 00:15:27 +0900

    add project

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+namespace Mobcast.Coffee.Api
+{
+	public static class ExportPackage
+	{
+		const string kPackageName = "ScrollEx.unitypackage";
+		static readonly string[] kAssetPathes = {
+			"Assets/Mobcast/Coffee/ScrollEx",
+		};
+
+		[MenuItem ("Export Package/" + kPackageName)]
+		[InitializeOnLoadMethod]
+		static void Export ()
+		{
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
+				return;
+			
+			AssetDatabase.ExportPackage (kAssetPathes, kPackageName, ExportPackageOptions.Recurse | ExportPackageOptions.Default);
+			UnityEngine.Debug.Log ("Export successfully : " + kPackageName);
+		}
+	}
+}
---
+++ Assets/Mobcast/Coffee/ScrollEx/Readme.md
@@ -0,0 +1,50 @@
+ScrollEx
+===
+
+## Overview
+
+ScrollEx
+
+
+
+
+## Requirement
+
+* Unity5.3+ *(included Unity 2017.x)*
+* No other SDK
+
+
+
+
+## Usage
+
+1. Download [ScrollEx.unitypackage](https://github.com/mob-sakai/ScrollEx/raw/master/ScrollEx.unitypackage) and install to your project.
+
+
+
+
+### スクロールコンポーネントと入れ替える
+
+
+
+
+### 機能一覧
+
+オブジェクトプール
+
+スナップ
+
+ネストスクロール
+
+無限スクロール
+
+
+
+
+
+
+## Release Notes
+
+### ver.0.1.0:
+
+* 
