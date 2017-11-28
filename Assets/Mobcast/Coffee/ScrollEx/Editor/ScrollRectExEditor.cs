1b62cfe1ad8dfdf28ce6a1ac2d80da5c0c05da44
Takashi Sakai <sakai@mobcast.jp>
2017-11-28 20:13:28 +0900

    てすと

---
+++ Assets/Mobcast/Coffee/ScrollEx/Editor/ScrollRectExEditor.cs
@@ -0,0 +1,23 @@
+﻿//using UnityEngine;
+//using UnityEditor;
+//using UnityEditor.UI;
+//using System.Collections;
+//
+//namespace Mobcast.Coffee
+//{
+//	[CustomEditor (typeof(ScrollRectEx), true)]
+//	public class ScrollRectExEditor : ScrollRectEditor
+//	{
+//		public override void OnInspectorGUI ()
+//		{
+//			base.OnInspectorGUI ();
+//
+//			serializedObject.Update ();
+//
+//			DrawDefaultInspector ();
+//
+//			serializedObject.ApplyModifiedProperties ();
+//		}
+//	}
+//
+//}
